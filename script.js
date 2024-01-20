9e99c566310ea39a437c8afce815bfb15a6bbba2
DoanSy <99658269+DoanSy16@users.noreply.github.com>
2024-01-20 21:17:31 +0700

    update profile

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function()
+{
+	$("html").niceScroll({
+		
+		cursorcolor:"#212b37",
+		cursorborder:"5px solid #212b37",
+		cursorborderradius:"0",
+		scrollspeed:65
+	})
+
+	$("div").filter("#scrolls").niceScroll({
+		
+		cursorcolor:"#212b37",
+		cursorborder:"2px solid #212b37",
+		cursorborderradius:"0",
+		scrollspeed:65
+	})
+	
+});
